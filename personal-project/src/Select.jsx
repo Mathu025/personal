3c399e8008cc40455e02fa3cd1df70ae197e20ec
Mathu025 <mathualex72@gmail.com>
2025-07-30 18:30:44 +0300

    Add css to filterbar

--- personal-project/src/Select.jsx
+++ personal-project/src/Select.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
+import "./Select.css"
 
 const Select = () => {
-  return (
+    return (
     <>
-    <input type="text" placeholder="Type in Game"/>
-    <div>
-        <select>
+    <div className='br'>
+    <input type="text" placeholder="Type in Game" className="input"/>
+    
+        <select className="select">
             <option value="">All Genres</option>
             <option value="Action">Action</option>
             <option value="Sports">Sports</option>
             <option value="Adventure">Adventure</option>
         </select>
 
-        <select>
+        <select className="select">
             <option value="">All Platforms</option>
             <option value="Xbox">Xbox</option>
             <option value="Pc">Pc</option>
             <option  value="PlayStation">PlayStation</option>
         </select>
         
+    
     </div>
     </>
     )
